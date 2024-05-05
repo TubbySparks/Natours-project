5b1d53ec72d3716443a815649f3e5ebab7d3d55d
Tubby Sparks <adeyemotunbosun20@gmail.com>
2024-05-05 03:11:02 +0100

    improved stripe implementation

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const webhookCheckout = require('./controller/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 // Start Express App
@@ -77,6 +78,12 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
